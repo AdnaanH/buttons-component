c9e0e78edac9b36307b43ad93daafad70aba401b
AdnaanH <60857004+AdnaanH@users.noreply.github.com>
2024-05-09 01:07:01 +0300

    Complete Button Sets

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,54 @@
+import Button from "./components/Button"
+import { FaStar } from "react-icons/fa"
+
 function App() {
 
   return (
     <main className="min-h-screen w-full flex justify-center items-center">
-     <h1 className="text-red font-semibold text-5xl">HELLO WORLD</h1>
+      <div className="w-11/12 flex flex-col gap-6 items-center">
+        <div className="flex flex-wrap justify- items-center gap-4">
+          <Button bg="bg-ButtonBlue" py="py-2">Button CTA</Button>
+          <Button bg="bg-ButtonBlue" py="py-3" leftIcon={<FaStar />}>Button CTA</Button>
+          <Button bg="bg-ButtonBlue" py="py-4" rightIcon={<FaStar />}>Button CTA</Button>
+          <Button bg="bg-ButtonBlue" py="py-5">Button CTA</Button>
+          <Button bg="bg-ButtonBlue" py="py-4" px="px-4" icon={<FaStar />}></Button>
+        </div>
+        <div className="flex flex-wrap justify-between items-center gap-4">
+          <Button bg="bg-ButtonRed" py="py-2">Button CTA</Button>
+          <Button bg="bg-ButtonRed" py="py-3" leftIcon={<FaStar />}>Button CTA</Button>
+          <Button bg="bg-ButtonRed" py="py-4" rightIcon={<FaStar />}>Button CTA</Button>
+          <Button bg="bg-ButtonRed" py="py-5">Button CTA</Button>
+          <Button bg="bg-ButtonRed" py="py-4" px="px-4" icon={<FaStar />}></Button>
+        </div>
+        <div className="flex flex-wrap justify-between items-center gap-4">
+          <Button bg="bg-white" text="text-black" py="py-2">Button CTA</Button>
+          <Button bg="bg-white" text="text-black" py="py-3" leftIcon={<FaStar />}>Button CTA</Button>
+          <Button bg="bg-white" text="text-black" py="py-4" rightIcon={<FaStar />}>Button CTA</Button>
+          <Button bg="bg-white" text="text-black" py="py-5">Button CTA</Button>
+          <Button bg="bg-white" text="text-black" py="py-4" px="px-4" icon={<FaStar />}></Button>
+        </div>
+        <div className="flex flex-wrap justify-between items-center gap-3">
+          <Button bg="bg-white" text="text-ButtonBlue" shadow="shadow-none" py="py-2">Button CTA</Button>
+          <Button bg="bg-white" text="text-ButtonBlue" shadow="shadow-none" py="py-3" leftIcon={<FaStar />}>Button CTA</Button>
+          <Button bg="bg-white" text="text-ButtonBlue" shadow="shadow-none" py="py-4" rightIcon={<FaStar />}>Button CTA</Button>
+          <Button bg="bg-white" text="text-ButtonBlue" shadow="shadow-none" py="py-5">Button CTA</Button>
+          <Button bg="bg-white" text="text-ButtonBlue" shadow="shadow-none" py="py-4" px="px-4" icon={<FaStar />}></Button>
+        </div>
+        <div className="flex flex-wrap justify-between items-center gap-3">
+          <Button bg="bg-white" text="text-ButtonBlue" shadow="shadow-none" px="px-3" py="py-2">Button CTA</Button>
+          <Button bg="bg-white" text="text-ButtonBlue" shadow="shadow-none" px="px-3" py="py-3" leftIcon={<FaStar />}>Button CTA</Button>
+          <Button bg="bg-white" text="text-ButtonBlue" shadow="shadow-none" px="px-3" py="py-4" rightIcon={<FaStar />}>Button CTA</Button>
+          <Button bg="bg-white" text="text-ButtonBlue" shadow="shadow-none" px="px-3" py="py-5">Button CTA</Button>
+          <Button bg="bg-white" text="text-ButtonBlue" shadow="shadow-none" py="py-4" px="px-4" icon={<FaStar />}></Button>
+        </div>
+        <div className="flex flex-wrap justify-between items-center gap-3">
+          <Button bg="bg-white" text="text-black" shadow="shadow-none" px="px-3" py="py-2">Button CTA</Button>
+          <Button bg="bg-white" text="text-black" shadow="shadow-none" px="px-3" py="py-3" leftIcon={<FaStar />}>Button CTA</Button>
+          <Button bg="bg-white" text="text-black" shadow="shadow-none" px="px-3" py="py-4" rightIcon={<FaStar />}>Button CTA</Button>
+          <Button bg="bg-white" text="text-black" shadow="shadow-none" px="px-3" py="py-5">Button CTA</Button>
+          <Button bg="bg-white" text="text-black" shadow="shadow-none" py="py-4" px="px-4" icon={<FaStar />}></Button>
+        </div>
+      </div>
     </main>      
   )
 }
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,11 @@
-const Button = () => {
+const Button = ({ children, bg = 'bg-black', text = 'text-white', w = 'full', shadow = 'shadow-md', leftIcon = null, rightIcon = null, icon = null, py = 'py-2', px = 'px-8' }) => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <button className={`${bg} ${text} ${w} ${px} ${py} ${shadow} hover:opacity-80 font-BeVietnam font-Noto font-normal text-base rounded-lg flex items-center justify-center`}>
+      {leftIcon && <span className="mr-2">{leftIcon}</span>}
+      {children} {icon}
+      {rightIcon && <span className="ml-2">{rightIcon}</span>}
+    </button>
+  );
+};
 
-export default Button
+export default Button;
